a016b53d5eff756a2b4756df4d419f0cd25a72a6
Nevenit <Nevenit@outlook.com>
2022-07-11 10:02:53 +1000

    I can't use git.

---
+++ ImageProcessing.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageComparison
+{
+    internal class ImageProcessing
+    {
+        public static Bitmap CropImage(Bitmap image, Rectangle rect)
+        {
+
+            Bitmap destImage = new Bitmap(rect.Width, rect.Height);
+            using (Graphics g = Graphics.FromImage(destImage))
+            {
+                g.DrawImage(image, -rect.X, -rect.Y);
+            }
+
+            return destImage;
+        }
+    }
+}
